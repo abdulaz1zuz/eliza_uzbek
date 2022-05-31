90fe94e77dbbd0d72478a7f50b36639693e62fce
abdulaz1zuz <azamat.aliqulov@list.ru>
2022-05-31 14:54:38 +0500

    initial

--- eliza.py
+++ eliza.py
@@ -1,14 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-#----------------------------------------------------------------------
-#  eliza.py
-#
-#  a cheezy little Eliza knock-off by Joe Strout
-#  with some updates by Jeff Epler
-#  hacked into a module and updated by Jez Higgins
-#----------------------------------------------------------------------
-
 import string
 import re
 import random
@@ -18,10 +7,6 @@ class Eliza:
     self.keys = list(map(lambda x: re.compile(x[0], re.IGNORECASE), gPats))
     self.values = list(map(lambda x: x[1], gPats))
 
-  #----------------------------------------------------------------------
-  # translate: take a string, replace any words found in vocabulary.keys()
-  #  with the corresponding vocabulary.values()
-  #----------------------------------------------------------------------
   def translate(self, text, vocabulary):
     words = text.lower().split()
     keys = vocabulary.keys();
@@ -30,20 +15,12 @@ class Eliza:
         words[i] = vocabulary[words[i]]
     return ' '.join(words)
 
-  #----------------------------------------------------------------------
-  #  respond: take a string, a set of regexps, and a corresponding
-  #    set of response lists; find a match, and return a randomly
-  #    chosen response from the corresponding list.
-  #----------------------------------------------------------------------
   def respond(self, text):
     # find a match among keys
     for i in range(0, len(self.keys)):
       match = self.keys[i].match(text)
       if match:
-        # found a match ... stuff with corresponding value
-        # chosen randomly from among the available options
         resp = random.choice(self.values[i])
-        # we've got a response... stuff in reflected text where indicated
         pos = resp.find('%')
         while pos > -1:
           num = int(resp[pos+1:pos+2])
@@ -51,39 +28,13 @@ class Eliza:
             self.translate(match.group(num), gReflections) + \
             resp[pos+2:]
           pos = resp.find('%')
-        # fix munged punctuation at the end
         if resp[-2:] == '?.': resp = resp[:-2] + '.'
         if resp[-2:] == '??': resp = resp[:-2] + '?'
         return resp
     return None
 
-#----------------------------------------------------------------------
-# gReflections, a translation table used to convert things you say
-#    into things the computer says back, e.g. "I am" --> "you are"
-#----------------------------------------------------------------------
-gReflections = {
-  "am"   : "are",
-  "was"  : "were",
-  "i"    : "you",
-  "i'd"  : "you would",
-  "i've"  : "you have",
-  "i'll"  : "you will",
-  "my"  : "your",
-  "are"  : "am",
-  "you've": "I have",
-  "you'll": "I will",
-  "your"  : "my",
-  "yours"  : "mine",
-  "you"  : "me",
-  "me"  : "you"
-}
 
-#----------------------------------------------------------------------
-# gPats, the main response table.  Each element of the list is a
-#  two-element list; the first is a regexp, and the second is a
-#  list of possible responses, with group-macros labelled as
-#  %1, %2, etc.
-#----------------------------------------------------------------------
+
 gPats = [
  [r'Menga (.*) kerak',
   [ "Nima uchun sizga %1 kerak?",
@@ -275,6 +226,21 @@ def command_interface():
       s = s[:-1]
     print(therapist.respond(s))
 
-
+gReflections = {
+  "am"   : "are",
+  "was"  : "were",
+  "i"    : "you",
+  "i'd"  : "you would",
+  "i've"  : "you have",
+  "i'll"  : "you will",
+  "my"  : "your",
+  "are"  : "am",
+  "you've": "I have",
+  "you'll": "I will",
+  "your"  : "my",
+  "yours"  : "mine",
+  "you"  : "me",
+  "me"  : "you"
+}
 if __name__ == "__main__":
   command_interface()
--- sum_of_money.py
+++
@@ -1,7 +0,0 @@
-from decimal import Decimal
-a=1.2
-sell_price = Decimal(a)
-buy_price = Decimal('232.99')
-items_sold = Decimal('1e18')
-profit = (sell_price - buy_price)
-print(sell_price)
